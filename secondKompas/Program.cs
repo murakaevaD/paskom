c119c916d5bbbb723399c8d9dbd423c4277ed24e
murakaevaD <daniamurakaeva@yandex.ru>
2022-10-03 17:54:11 +0300

    Добавьте файлы проекта.

--- secondKompas/Program.cs
+++ secondKompas/Program.cs
@@ -10,7 +10,7 @@ namespace secondKompas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SecondKompas");
+            Console.WriteLine("Хочу баунти");
         }
     }
 }
